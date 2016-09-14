841116aaf889529fd64b2271accd9e6b59312f37
Arne Schwabe <arne@rfc2549.org>
2016-09-14 22:57:58 +0200

    Hacks to make break pad compile under Android again ...

--- src/common/android/include/sys/user.h
+++ src/common/android/include/sys/user.h
@@ -65,13 +65,13 @@ typedef struct user_fxsr_struct user_fpxregs_struct;
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
-struct user_regs_struct {
+struct xxx_user_regs_struct {
  __u64 regs[31];
  __u64 sp;
  __u64 pc;
  __u64 pstate;
 };
-struct user_fpsimd_struct {
+struct xxx_user_fpsimd_struct {
  __uint128_t vregs[32];
  __u32 fpsr;
  __u32 fpcr;
